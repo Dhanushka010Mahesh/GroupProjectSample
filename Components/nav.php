0b8892183696da4a93c3bb770f3f3a2cd419ec97
Dhanushka010Mahesh <E2240438@bit.uom.lk>
2024-09-01 13:29:59 +0530

    correct dinamic changes page

--- Components/nav.php
+++ Components/nav.php
@@ -1,11 +1,9 @@
 
 <div class="bg-gray-900 p-4 text-white">
     <ul class="flex gap-5">
-        <li><a href="#" onclick="loadContent('Index')">Home</a></li>
-        <li><a href="#" onclick="loadContent('../Pages/AddProduct')">Add Product</a></li>
-        <li><a href="#" onclick="loadContent('ListProduct')">Product List</a></li>
-        <li><a href="#" onclick="loadContent('Cart')">Cart List</a></li>
+        <li><a href="#" onclick="loadContent('../Pages', 'Index')">Home</a></li>
+        <li><a href="#" onclick="loadContent('Pages', 'AddProduct')">Add Product</a></li>
+        <li><a href="#" onclick="loadContent('Pages', 'ListProduct')">Product List</a></li>
+        <li><a href="#" onclick="loadContent('Pages', 'Cart')">Cart List</a></li>
     </ul>
 </div>
-        
-        
--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
-function loadContent(page) {
+
+function loadContent(directory, pageName) {
     var xhr = new XMLHttpRequest();
-    xhr.open('GET',page + '.php', true);
+    // Construct the URL using directory and page name
+    var url = directory + '/' + pageName + '.php';
+
+    xhr.open('GET', url, true);
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
             document.getElementById('main-content').innerHTML = xhr.responseText;
@@ -8,7 +12,9 @@ function loadContent(page) {
     };
     xhr.send();
 }
+
 // Load default content on page load
 document.addEventListener('DOMContentLoaded', function() {
-    loadContent('../Pages/Cart');
+    // Example default content, adjust as needed
+    loadContent('Pages', 'Index'); // Default page
 });
