0c160e6761b70900722dfb62753eddc1be63888a
Kobopele Gedara Dhanushka Mahesh Eranga Jayarathna <158093265+Dhanushka010Mahesh@users.noreply.github.com>
2024-09-01 09:53:42 +0530

    Create file

---
+++ Sampale Projact/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="css/Tailwind_css.css">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+        include_once("Components/nav.php");
+
+    ?>
+    
+    <p>this sampale group project</p>
+
+    <?php
+
+        include_once("Components/foot.php");
+
+    ?>
+</body>
+</html>
