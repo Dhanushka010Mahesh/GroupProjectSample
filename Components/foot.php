86b82503f1617b216f31a069db4d89bde2cd0fc5
Dhanushka010Mahesh <E2240438@bit.uom.lk>
2024-09-06 23:55:03 +0530

    Create sign up

--- Components/foot.php
+++ Components/foot.php
@@ -1,4 +1,4 @@
-<div class="text-green-300 bg-gray-700">
+<div class="text-green-300 bg-gray-800">
     <div class="text-center p-10">
         <p>&copy; Group Project</p>
         <p class="text-sm">Contact - 0766640384</p>
