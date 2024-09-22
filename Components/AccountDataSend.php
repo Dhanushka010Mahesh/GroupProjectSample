9949fbf846ad2bfa64213f0283cefaa35320dee2
Dhanushka010Mahesh <E2240438@bit.uom.lk>
2024-09-23 01:04:16 +0530

    otp send create error

--- Components/AccountDataSend.php
+++ Components/AccountDataSend.php
@@ -1,27 +1,89 @@
 <?php
+
+$accountData = array(
+    "firstname" => "",
+    "lastname" => "",
+    "email" => "",
+    "password" => "",
+    "otp" => "",
+);
+
 if (isset($_POST['submitACC'])) {
     // Validate inputs
     if (empty($_POST['fname']) || empty($_POST['lname']) || empty($_POST['email']) || empty($_POST['password'])) {
         header("Location: ../index.php?signup=empty");
         exit();
+    }else{
+
+        $accountData["firstname"] = $_POST['fname'];
+        $accountData["lastname"] = $_POST['lname'];
+        $mailEmail=$_POST['email'];
+        $accountData["email"] = $mailEmail;
+        $accountData["password"] = password_hash($_POST['password'], PASSWORD_DEFAULT);
+        $newOtp=rand(0000,9999);
+        $accountData["otp"] = $newOtp;
+
+        require('Mail/PHPMailerAutoload.php');
+        $mailServer1=new PHPMailer;
+
+
+        $mailServer1->Host='smtp.gmail.com';
+        $mailServer1->Port=587;
+        $mailServer1->SMTPAuth=true;
+        $mailServer1->SMTPSecure='tls';
+
+        $myMailAddress='';
+        $mailServer1->Username=$myMailAddress;
+        $mailServer1->Password='';
+
+        $mailServer1->setFrom($myMailAddress,'dhanushka');
+        $mailServer1->addAddress($mailEmail);
+        
+
+        $mailServer1->isHTML(true);
+        $mailServer1->Subject="Your Verify code";
+        $mailServer1->Body="<p><b>User Name : </b> $mailEmail </p> <br> <h2>Your Verify Code : $newOtp <br></h2> ";
+
+        if($mailServer1->send()){
+            ?>
+
+                <script>
+                    alert('Invalid Email Address');
+                </script>
+
+            <?php
+        }else{
+            echo  "<script>alert('OTP code has been sending email $mailEmail');</script>";
+        }
+
     }
 
-    $fName = $_POST['fname'];
-    $lName = $_POST['lname'];
-    $email = $_POST['email'];
-    $Password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+   
 
-    // Connect to the database
+    
+} else {
+    header("Location: ../index.php?signup=empty");
+    exit();
+}
+
+if (isset($_POST['SubmitOTP'])) {
+    
+    $otp = $_POST['otpCode'];
+    if($otp != $accountData["otp"]){
+        echo  "<script>alert('Invalid OTP code');</script>";
+    }else{
+        // Connect to the database
     include_once("Connection.php");
 
     // Prepare the SQL query
     $stmt = $connect->prepare("INSERT INTO account (Fname, Lname, email, accPassword) VALUES (?, ?, ?, ?)");
-    $stmt->bind_param("ssss", $fName, $lName, $email, $Password);
+    $stmt->bind_param("ssss", $accountData["firstname"] , $accountData["lastname"] , $accountData["email"] , $accountData["password"] );
 
     // Execute the query
     if ($stmt->execute()) {
         // Optionally, set session variables or redirect
         header("Location: ../index.php?signup=success");
+        
     } else {
         header("Location: ../index.php?signup=error");
     }
@@ -29,7 +91,6 @@ if (isset($_POST['submitACC'])) {
     // Close the statement and connection
     $stmt->close();
     $connect->close();
-} else {
-    header("Location: ../index.php?signup=empty");
-    exit();
+    }
+
 }
--- Components/SignUp.php
+++ Components/SignUp.php
@@ -4,6 +4,8 @@
             Create a New Account
         </h1>
         <form class="space-y-4 md:space-y-6" method="POST" action="Components/AccountDataSend.php">
+            
+            <div class="otpVerifyDisplay1">
             <div>
                 <label for="fname" class="block mb-2 text-sm font-medium text-gray-900">First Name</label>
                 <input type="text" name="fname" id="fname" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="first name" required="">
@@ -21,7 +23,15 @@
                 <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required="">
             </div>
 
-            <button type="submit" name="submitACC" class="w-full text-white bg-gray-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign Up</button>
+            <button type="submit" name="submitACC" class="w-full text-white bg-gray-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Verify</button>
+            </div>
+               
+            <div class="otpVerifyDisplay2" >
+                <label for="otpCode">OTP Verify</label><br>
+                <input type="text" name="otpCode" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="OTP Code" required><br>
+                <button id="otp-btnSignup" type="submit" name="SubmitOTP" class="w-full text-white bg-gray-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center" >Create Account</button>
+            </div>
+
             <p class="text-sm font-light text-gray-500">
                 Already have an account? <a href="#" class="font-medium text-primary-600 hover:underline" onclick="loadContent('Components', 'SignIn','left-content')">Sign In</a>
             </p>
