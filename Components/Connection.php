2b0394f41746b0b2bba3dba77aebf183d4cc7dc4
Dhanushka010Mahesh <E2240438@bit.uom.lk>
2024-09-30 13:39:06 +0530

    delete all file

--- Components/Connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $connect = mysqli_connect("localhost", "root", "", "cloth_database");
-
-    if ($connect === false) {
-        die("Connection Error: " . mysqli_connect_error());
-    }
-?>
